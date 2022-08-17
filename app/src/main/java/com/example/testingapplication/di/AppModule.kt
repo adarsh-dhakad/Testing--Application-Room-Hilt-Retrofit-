e01c6d233a5f67a49fa535d68099fe64e0159504
Adarsh dhakad <58937745+adarsh-dhakad@users.noreply.github.com>
2022-08-18 01:31:44 +0530

    app mudule

--- app/src/main/java/com/example/testingapplication/di/AppModule.kt
+++ app/src/main/java/com/example/testingapplication/di/AppModule.kt
@@ -1,4 +1,46 @@
 package com.example.testingapplication.di
 
+import android.content.Context
+import androidx.room.Room
+import com.example.testingapplication.data.local.ShoppingItemDatabase
+import com.example.testingapplication.data.remote.PixabayAPI
+import com.example.testingapplication.other.Constants.BASE_URL
+import com.example.testingapplication.other.Constants.DATABASE_NAME
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.internal.modules.ApplicationContextModule
+import dagger.hilt.android.qualifiers.ApplicationContext
+import dagger.hilt.components.SingletonComponent
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+//@InstallIn(ApplicationContextModule)
+@InstallIn(SingletonComponent::class)
 object AppModule {
+
+    @Singleton
+    @Provides
+    fun provideShoppingItemDatabase(
+        @ApplicationContext context: Context
+    ) = Room.databaseBuilder(context,ShoppingItemDatabase::class.java,DATABASE_NAME).build()
+
+    @Singleton
+    @Provides
+    fun provideShoppingDao(
+        database: ShoppingItemDatabase
+    ) = database.shoppingDao()
+
+    @Singleton
+    @Provides
+    fun providePixabayApi():PixabayAPI{
+        return Retrofit.Builder()
+            .addConverterFactory(GsonConverterFactory.create())
+            .baseUrl(BASE_URL)
+            .build()
+            .create(PixabayAPI::class.java)
+    }
 }
